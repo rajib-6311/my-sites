dc9ed5e73a35e010231952f291b23fb5c1cdd835
rajibhossen <mhossen6311@gmail.com>
2024-06-29 13:16:49 +0600

    Contact update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,16 +1,17 @@
+import ContactAbout from "./ContactAbout";
 
 
 const Contact = () => {
     return (
         <div className="relative ">
-            <img className="h-[370px] w-full object-cover" src="https://i.ibb.co/N6Nxk6Z/bg12.jpg" alt="" />
+            <img className="h-[370px] w-full object-cover" src="https://i.ibb.co/6sBGWcF/bg16.jpg" alt="" />
             <div className="absolute inset-0 bg-gray-700 opacity-60 rounded-md">
 
             </div>
             <div className="absolute inset-0 flex items-center justify-center">
-           <h2 className="text-white text-6xl font-bold">Contact</h2>
+           <h2 className="text-white text-6xl font-bold pb-[300px]">Contact</h2>
             </div>
-            
+            <ContactAbout></ContactAbout>
       </div>
     );
 };
---
+++ src/Components/Contact/ContactAbout.jsx
@@ -0,0 +1,12 @@
+
+
+const ContactAbout = () => {
+    return (
+        <div className="bg-white pl-28 h-[300px] space-y-8 pt-10">
+            <h1 className="text-xl font-bold text-black">Email : mhossen191244@bscse.uiu.ac.bd</h1>
+            <h1 className="text-xl font-bold text-black">Socials : </h1>
+        </div>
+    );
+};
+
+export default ContactAbout;
