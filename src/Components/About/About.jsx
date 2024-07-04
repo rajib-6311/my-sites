fe688bfba2e422b0eb7ef6be9e08fd5ae3719fdb
rajibhossen <mhossen6311@gmail.com>
2024-07-04 22:55:47 +0600

    Home page responsive update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,11 +7,13 @@ const About = () => {
   return (
     <div className="hero bg-white">
       <div className="hero-content flex-col lg:flex-row">
+        <div  className='sm:max-w-sm lg:w-[2150px]'>
         <img data-aos="zoom-in" data-aos-duration="2000"
           src="https://i.ibb.co/ZmxrgrJ/ra.png"
-          className="max-w-sm  shadow-2xl"
+          className="  shadow-2xl"
         />
-        <div className="pl-3">
+        </div>
+        <div className="lg:pl-3 sm:pl-0">
         <h1 className="text-xl font-bold text-black">Hello, this is Md. Rajib Hossen, a Senior Software Engineer at Samsung Electronics.
         I am the current lead developer of Samsung Electronics’ internal CDN system - ADS (Artifact Delivery Service). Additionally, I am the secondary admin & product owner of the ADS service globally. 
         ADS uses a combination of proprietary network accelerators, smart caching, proxies and public cloud to provide artifacts faster and more securely to all Samsung employees & partners. 
